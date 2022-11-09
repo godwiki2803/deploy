e69ccdfbec90e4d3255a60a794472391a1acc02f
godwiki2803 <brayancardozod@gmail.com>
2022-11-08 20:05:43 -0500

    .
    .

---
+++ engine.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { Server } = require("socket.io");
+const app = express();
+const http = require('https');
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: ["https://jobbi.mx", "https://beta.jobbi.mx", "https://www.beta.jobbi.mx"],
+        //origin: ["http://localhost", "http://localhost:8080", "http://127.0.0.1", "http://127.0.0.1:8080"],
+        credentials: true
+    }
+});
+var server_port = 443;
+
+io.on('connection', (socket) => { 
+  console.log("Un usuario a ingresado a la plataforma.");
+  
+  socket.on("notifications" , (value) => {
+    console.log("Notificacion recibida: " + value);
+    io.emit("notifications", value);
+  });
+
+  socket.on('new offer' , (offer_signal) => {
+    console.log("Nueva oferta para: "+ offer_signal);
+    io.emit('new offer' , offer_signal);
+  });
+  socket.on('new service' , (offer_for) =>{
+    console.log("Nuevo servicio requerido para el area de: " + offer_for );
+    io.emit('new service' , offer_for);
+  });
+
+  socket.on('app_msg', (msg) => {
+    // console.log("----------- MENSAJE -----------");
+    // console.log(msg.ns);
+    // console.log(msg.user);
+    // console.log(msg.content);
+    // console.log(msg.file);
+    // console.log("-------------------------------");
+    io.emit('app_msg', msg);
+  });
+});
+
+server.listen(server_port, () => {
+  console.log('Servicor escuchando en el puerto: ' + server_port);
+});
