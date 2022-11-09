18a2b3f6ddfccc201891e515189dbdc650c636a0
godwiki2803 <brayancardozod@gmail.com>
2022-11-08 22:26:02 -0500

    .
    .

--- engine.js
+++ engine.js
@@ -5,7 +5,7 @@ const http = require('https');
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["https://jobbi.mx", "https://beta.jobbi.mx", "https://www.beta.jobbi.mx"],
+      origin: '*',
         //origin: ["http://localhost", "http://localhost:8080", "http://127.0.0.1", "http://127.0.0.1:8080"],
         credentials: true
     }
